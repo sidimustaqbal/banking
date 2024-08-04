2e37d0c0dcbb914fd235fa077a7784a3786cc38a
sidi mustaqbal <sidi.mustaqbal@gmail.com>
2024-08-04 07:36:06 +0700

    implement headerbox and chartjs

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,27 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstname: "Muhammad" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <HeaderBox
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstname || "Guest"}
+          subtext="Access and manage your account and transaction efficiently."
+        />
+
+        <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={123.21}
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Home;
